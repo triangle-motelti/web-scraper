e316b7a33fe968bfd59e030277d31cd97e11858c
motelti <mteltipr03@gmail.com>
2025-09-01 16:25:18 +0100

    fixed some bugs in get_interactive_input

--- interactive_in.py
+++ interactive_in.py
@@ -5,8 +5,7 @@ def get_interactive_input(prompt):
     if sys.stdin.isatty():
         return input(prompt).strip()
     else:
-        try:
-            with open('/dev/tty', 'r') as tty:
-                sys.stdout.write(prompt)
-                sys.stdout.flush()
-                return tty.readline().strip()
+        with open('/dev/tty', 'r') as tty:
+            sys.stdout.write(prompt)
+            sys.stdout.flush()
+            return tty.readline().strip()
